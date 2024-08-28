5c2d5c5894313bea1ea6055a39688b963f553e1e
Abdul Wahab Tahir <133004396+awahabt@users.noreply.github.com>
2024-08-28 21:47:26 +0500

    add new design code

---
+++ script.js
@@ -0,0 +1,16 @@
+const range = document.querySelector(".slider-container .price");
+
+const rangeInput = document.querySelectorAll(".input-range input")
+
+
+let priceGap =  500;
+
+
+const priceInputValue = document.querySelector(".input-price input")
+
+for( let i=0; i < priceInputValue.length; i++){
+    priceInputValue[i].addEventListener("input", e => {
+
+        
+    })
+}
